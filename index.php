3c661b3016cb5712f309f38e5bc888588893be8b
Arjun Rajan <rajan.arjun@gmail.com>
2021-04-20 21:20:22 +0530

    initial commit

---
+++ index.php
@@ -0,0 +1,22 @@
+<html>
+    <head></head>
+    <body>
+        <div class="container">
+            <div class="row">
+                <div class="col-md-12">
+                    <form>
+                        <div class="form-group">
+                            <label for="inputFullName">Full Name</label>
+                            <input type="text" class="form-control" id="inputFullName" aria-describedby="nameHelp" placeholder="Enter Full Name">
+                        </div>
+                        <div class="form-group">
+                            <label for="inputEmail">Email address</label>
+                            <input type="text" class="form-control" id="inputEmail" aria-describedby="emailHelp" placeholder="Enter Email">
+                        </div>
+                    </form>  
+                    
+                </div>
+            </div>
+        </div>
+    </body>
+</html>
